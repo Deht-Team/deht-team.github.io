b726867f72a83040a457976f0a39bdda4f5dead3
AbdulSamad <abdulsamad.arilewola@gmail.com>
2026-04-22 16:52:25 +0100

    Add Index component import and Route and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Index from './pages/Index.tsx'
 
 export default function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
+       <Route exact path="/" element={<Index />}>
       </Routes>
     </BrowserRouter>
   );
