624626c43684ed14ffae439d425812d5451013b9
IamBobo <abdulsamad.arilewola@gmail.com>
2026-05-23 12:10:32 +0100

    Add Auth route to the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Index from "./pages/Index"
+import Auth from "./pages/Auth"
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer"
 
@@ -12,6 +13,7 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
 
       <Footer />
